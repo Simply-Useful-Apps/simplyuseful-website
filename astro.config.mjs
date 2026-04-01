2020c37bc26e80cf925e886857aa2c3a17f2e04f
Michael Tucker <tuckerm@gmail.com>
2026-04-01 17:48:22 -0400

    Added site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://simplyuseful.app',
 	image: {
     responsiveStyles: true,
     layout: 'constrained',
